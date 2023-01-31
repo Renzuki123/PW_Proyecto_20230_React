ec4589a96a2b3963ad45ca40a2893ac2981d5b6e
L34204 <L34204@lab.aca>
2023-01-31 18:29:24 -0500

    v1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Restaurant from './Restaurantes/restaurant.jsx';
+
+ReactDOM.render(<Restaurant />, document.getElementById('root'));
