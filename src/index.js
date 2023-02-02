19ff1650e75b989750a287b851ff3b5dff53ee0c
RenzoCavero <renzo.sebastian.cavero.canchan@gmail.com>
2023-02-02 00:40:36 -0500

    Req2 Agregado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import LoginPage from './Login/LoginPage';
 import MainPage from './MainPage/MainPage';
-import App2 from './Carta/App2'
+import Req2 from './Req2/Req2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,7 +16,7 @@ root.render(
         <Route path='/Login' element={ <LoginPage/> } />
         <Route path='/Carrito' element={ <carrito/> } />
         <Route path='/restaurant' element={ <restaurant/> } />
-        <Route path='/MarcosBistro' element = {<App2/>}/>
+        <Route path='/Req2' element = {<Req2/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
