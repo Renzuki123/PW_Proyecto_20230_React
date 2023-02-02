e8c20c4dca867dba9ae39e49a890074cd9926ff3
Renzuki123 <renzosaucedo@hotmail.com>
2023-02-02 01:44:51 -0500

    registro_pedido añadido

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import LoginPage from './Login/LoginPage';
 import MainPage from './MainPage/MainPage';
 import Req2 from './Req2/Req2';
-import App2 from './Carta/App2';
 import Restaurant from './Restaurantes/restaurant';
-import estado_pedido from '.\Restaurantes\estado_pedido';
+import Estado_pedido from './Restaurantes/estado_pedido';
+import Registro_pedido from './Restaurantes/registro_pedido'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,12 +19,12 @@ root.render(
         <Route path='/Login' element={ <LoginPage/> } />
         <Route path='/Carrito' element={ <carrito/> } />
         <Route path='/Restaurantes/restaurant' element={ <Restaurant/> } />
-        <Route path='/MarcosBistro' element = {<App2/>}/>
-        <Route path='/Restaurantes/estado_pedido' element = {<estado_pedido/>}/>
-        <Route path='/restaurant' element={ <restaurant/> } />
+        <Route path= '/Restaurantes/estado_pedido' element={ <Estado_pedido/>}/>
         <Route path='/Req2' element = {<Req2/>}/>
+        <Route path='/Restaurantes/registro_pedido' element = {<Registro_pedido/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
+//\Restaurantes\estado_pedido.jsx
